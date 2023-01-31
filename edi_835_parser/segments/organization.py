c713316d896304a7937e0216dc5b130c902d127c
shouraykumra <shouraykumra>
2023-01-31 22:19:43 +0000

    correction in organization segment

--- edi_835_parser/segments/organization.py
+++ edi_835_parser/segments/organization.py
@@ -1,29 +1,26 @@
 from edi_835_parser.elements.identifier import Identifier
 from edi_835_parser.elements.organization_type import OrganizationType
-from edi_835_parser.segments.utilities import split_segment
+from edi_835_parser.segments.utilities import split_segment, get_element
 
 
 class Organization:
-	identification = 'N1'
-
-	identifier = Identifier()
-	type = OrganizationType()
-
-	def __init__(self, segment: str):
-		self.segment = segment
-		segment = split_segment(segment)
-
-		self.identifier = segment[0]
-		self.type = segment[1]
-		self.name = segment[2]
+    identification = 'N1'
+    
+    identifier = Identifier()
+    type = OrganizationType()
+    
+    def __init__(self, segment: str):
+        self.segment = segment
+        segment = split_segment(segment)
         
-        #this could be modified.
-        if self.type=='PE':
-            self.billingNPI = segment[4]
-
-	def __repr__(self):
-		return '\n'.join(str(item) for item in self.__dict__.items())
-
-
+        self.identifier = segment[0]
+        self.type = segment[1]
+        self.name = segment[2]
+        self.billingNPI = get_element(segment, 4)
+                    
+    def __repr__(self):
+        return '\n'.join(str(item) for item in self.__dict__.items())
+        
+        
 if __name__ == '__main__':
-	pass
+    pass
