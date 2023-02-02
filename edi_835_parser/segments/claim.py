3330d0e724fc6c93b532151528dde4e058560bf8
shouraykumra <shouraykumra>
2023-02-02 17:13:13 +0000

    spacing issue

--- edi_835_parser/segments/claim.py
+++ edi_835_parser/segments/claim.py
@@ -5,27 +5,27 @@ from edi_835_parser.segments.utilities import split_segment, get_element
 
 
 class Claim:
-	identification = 'CLP'
+    identification = 'CLP'
 
-	identifier = Identifier()
-	status = ClaimStatus()
-	charge_amount = Dollars()
-	paid_amount = Dollars()
+    identifier = Identifier()
+    status = ClaimStatus()
+    charge_amount = Dollars()
+    paid_amount = Dollars()
 
-	def __init__(self, segment: str):
-		self.segment = segment
-		segment = split_segment(segment)
+    def __init__(self, segment: str):
+        self.segment = segment
+        segment = split_segment(segment)
 
-		self.identifier = segment[0]
-		self.marker = segment[1]
-		self.status = segment[2]
-		self.charge_amount = segment[3]
-		self.paid_amount = segment[4]
+        self.identifier = segment[0]
+        self.marker = segment[1]
+        self.status = segment[2]
+        self.charge_amount = segment[3]
+        self.paid_amount = segment[4]
         self.revenue_code = get_element(segment, 6)
 
-	def __repr__(self):
-		return '\n'.join(str(item) for item in self.__dict__.items())
+    def __repr__(self):
+        return '\n'.join(str(item) for item in self.__dict__.items())
 
 
 if __name__ == '__main__':
-	pass
+    pass
