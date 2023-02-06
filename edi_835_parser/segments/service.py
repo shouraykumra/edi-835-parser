f070f7e8a3769458014fa9e90508be106d2a9d24
shouraykumra <shouraykumra>
2023-02-06 18:11:29 +0000

    pushing all the changes

--- edi_835_parser/segments/service.py
+++ edi_835_parser/segments/service.py
@@ -8,37 +8,37 @@ from edi_835_parser.segments.utilities import split_segment, get_element
 
 
 class Service:
-	identification = 'SVC'
+    identification = 'SVC'
 
-	identifier = Identifier()
-	charge_amount = Dollars()
-	paid_amount = Dollars()
-	code = ServiceCode()
-	qualifier = ServiceQualifer()
-	modifier = ServiceModifier()
-	allowed_units = Integer()
-	billed_units = Integer()
+    identifier = Identifier()
+    charge_amount = Dollars()
+    paid_amount = Dollars()
+    code = ServiceCode()
+    qualifier = ServiceQualifer()
+    modifier = ServiceModifier()
+    allowed_units = Integer()
+    billed_units = Integer()
 
-	def __init__(self, segment: str):
-		self.segment = segment
-		segment = split_segment(segment)
+    def __init__(self, segment: str):
+        self.segment = segment
+        segment = split_segment(segment)
 
-		self.identifier = segment[0]
-		self.code = segment[1]
-		self.qualifier = segment[1]
-		self.modifier = segment[1]
-		self.charge_amount = segment[2]
-		self.paid_amount = segment[3]
+        self.identifier = segment[0]
+        self.code = segment[1]
+        self.qualifier = segment[1]
+        self.modifier = segment[1]
+        self.charge_amount = segment[2]
+        self.paid_amount = segment[3]
+        self.revenue_code = get_element(segment, 4)
+        # assume unit count of one if unit not provided
+        default = 0 if self.paid_amount == 0 else 1
+        self.allowed_units = get_element(segment, 5, default=default)
 
-		# assume unit count of one if unit not provided
-		default = 0 if self.paid_amount == 0 else 1
-		self.allowed_units = get_element(segment, 5, default=default)
+        self.billed_units = get_element(segment, 7, default=self.allowed_units)
 
-		self.billed_units = get_element(segment, 7, default=self.allowed_units)
-
-	def __repr__(self):
-		return '\n'.join(str(item) for item in self.__dict__.items())
+    def __repr__(self):
+        return '\n'.join(str(item) for item in self.__dict__.items())
 
 
 if __name__ == '__main__':
-	pass
+    pass
